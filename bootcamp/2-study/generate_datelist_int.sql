c4fd206479bd6f7b36d093064a2c69796d63b753
anologicon <pauloraimundi2009@gmail.com>
2025-06-30 16:47:02 -0300

    feat: refactor SQL script for generating user device activity datelist

--- bootcamp/2-study/generate_datelist_int.sql
+++ bootcamp/2-study/generate_datelist_int.sql
@@ -1,27 +1,41 @@
-drop table user_devices_datelist_int;
-create table user_devices_datelist_int (
-	user_id text,
-	datelist_int json,
-	date_ref date
+DROP TABLE user_devices_datelist_int;
+CREATE TABLE user_devices_datelist_int (
+	user_id TEXT,
+	datelist_int JSON,
+	date_ref DATE
 );
-
-insert into user_devices_datelist_int
-with date_series as (
-	SELECT generate_series('2022-12-31', '2023-01-31', INTERVAL '1 day') AS valid_date
-)
-SELECT 
-	user_id,
-	JSONB_OBJECT_AGG(key,(
-		with pow_cte as (
-		  	select sum(
-		  	case when array((select jsonb_array_elements_text(uc.device_activity_datelist -> key)::date)) @> array[date(d.valid_date)]
-		  	then POW(2,32 - EXTRACT(DAY FROM DATE('2023-01-31') - d.valid_date)) else 0 end)::bigint::bit(32) bit_date
-		  	from date_series as d
+INSERT INTO user_devices_datelist_int
+WITH date_series AS (
+		SELECT GENERATE_SERIES('2022-12-31', '2023-01-31', INTERVAL '1 day') AS valid_date
+	)
+SELECT user_id,
+	JSONB_OBJECT_AGG(
+		key,
+		(
+			WITH pow_cte AS (
+				SELECT SUM(
+						CASE
+							WHEN ARRAY(
+								(
+									SELECT JSONB_ARRAY_ELEMENTS_TEXT(uc.device_activity_datelist->key)::DATE
+								)
+							) @> ARRAY [DATE(d.valid_date)] THEN POW(
+								2,
+								32 - EXTRACT(
+									DAY
+									FROM DATE('2023-01-31') - d.valid_date
+								)
+							)
+							ELSE 0
+						END
+					)::BIGINT::BIT(32) bit_date
+				FROM date_series AS d
+			)
+			SELECT bit_date
+			FROM pow_cte
 		)
-		select  bit_date from pow_cte
-	)) as datelist_int,
-	DATE('2023-01-31') as date_ref
-	
-	FROM user_devices_cumulated uc, JSONB_OBJECT_KEYS(device_activity_datelist) AS key
-	group by user_id
-
+	) AS datelist_int,
+	DATE('2023-01-31') AS date_ref
+FROM user_devices_cumulated uc,
+	JSONB_OBJECT_KEYS(device_activity_datelist) AS key
+GROUP BY user_id;
